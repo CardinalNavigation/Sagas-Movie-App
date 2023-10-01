3d9b672ed046f93529c7746ec78178f3e9ba1bdf
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-10-01 15:06:39 -0500

    squashed a bug with the display of items out of the array. Needed to run a function to match up that params.id with the ID of the movie displaying in the details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,37 +3,57 @@ import { useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
-function Details(props) {
+function Details() {
 
     const movies = useSelector(store => store.movies);
     const genres = useSelector(store => store.genres);
     const params = useParams();
-    // console.log(params)
-    console.log("Genres Arrray", genres)
+    console.log("Params .Id:", params.id)
+    // console.log("Props Key:", props)
+    // console.log("Genres Arrray:", genres)
+    console.log("Movies Arrray:", movies)
+    // console.log("Digging Into Movies:", movies[0])
 
-    let currentMovieId = params.id - 1
-    console.log(currentMovieId)
-    let currentMovie = movies[currentMovieId]
-    console.log(currentMovie)
+    /* Params Starts with 1, so we are doing params.id-1 to get something from our array
+    // However the array is not in sequential ID order since it is pulled from the DB by Name
+    alphabetically. We need to find an alternative that pulls the information by ID#
+    One option is to loop over the array and get all the id's out. (MAP?) 
+    We went for a array.find() method to match up the params and the ID so they tracked with eachother**/
+
+    const findIDFunction = (movie) => {
+        // console.log(movie.id)
+        // console.log(params.id)
+        return movie.id == params.id
+    }
+
+    const currentMovie = movies.find(findIDFunction)
+    console.log("Find Correct Movie ID:", currentMovie)
+
+    // Below did NOT work:
+    // let  currentMovie= params.id - 1
+    // console.log("Current Movie", currentMovieId)
+    // let currentMovie = movies[currentMovieId]
+    // console.log("Current Movie:", currentMovie)
 
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
-        dispatch({ type: 'FETCH_GENRES', payload: params.id});
+        dispatch({ type: 'FETCH_GENRES', payload: params.id });
     }, []);
 
 
     return (
         <>
             <div>
-                <h2>Details</h2>
+                <h1>Details</h1>
+                <img src={currentMovie.poster}></img>
                 <h3>Genres:</h3>
                 {genres.map(genre => {
                     return (
-                      <p>{genre.name}</p>
+                        <p>{genre.name}</p>
                     );
                 })}
-                <img src={currentMovie.poster}></img>
+                <h3>Description:</h3>
                 <p>{currentMovie.description}</p>
             </div>
             <Link to={`/`}>
